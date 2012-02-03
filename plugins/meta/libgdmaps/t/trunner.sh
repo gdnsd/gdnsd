e932c8eb036ba855b7f0efb549e156cc53fc20a7
Brandon Black <blblack@gmail.com>
2012-02-03 02:34:56 -0500

    update libgdmaps testsuite to save outputs

--- plugins/meta/libgdmaps/t/trunner.sh
+++ plugins/meta/libgdmaps/t/trunner.sh
@@ -2,7 +2,7 @@
 
 skip_all=0
 for gdata in $GEOLITE_FILES; do
-    if [ ! -f $BDIR/$gdata ]; then
+    if [ ! -f $ABDIR/$gdata ]; then
         skip_all=1
     fi
 done
@@ -12,14 +12,17 @@ if [ $skip_all -eq 1 ]; then
     exit 0
 fi
 
+echo "Test outputs will be stored in $ABDIR/testout"
+mkdir -p $ABDIR/testout >/dev/null 2>&1
+
 for tnam in $TLIST; do
     echo "Running test $tnam ..."
-    cp $SDIR/$tnam.cfg $BDIR/ >/dev/null 2>&1
-    $BDIR/$tnam.bin $BDIR/$tnam.cfg >/dev/null 2>&1
+    cp $ASDIR/$tnam.cfg $ABDIR/ >/dev/null 2>&1
+    $ABDIR/$tnam.bin $ABDIR/$tnam.cfg >$ABDIR/testout/$tnam.out 2>&1
     rv=$?
     if [ $rv -ne 0 ]; then
-        echo "Test $tnam failed w/ exit status $rv; re-running with full output...";
-        $BDIR/$tnam.bin $SDIR/$tnam.cfg
+        echo "Test $tnam failed w/ exit status $rv; Test Output:"
+        cat $ABDIR/testout/$tnam.out
         exit 99
     fi
 done
