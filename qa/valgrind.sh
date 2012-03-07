fe20543eb35b3ca81fff4b885103f22b62a8f481
Brandon Black <blblack@gmail.com>
2012-03-06 23:49:50 -0500

    move gdnsd.supp to qa/ directory, where it belongs

--- qa/valgrind.sh
+++ qa/valgrind.sh
@@ -4,4 +4,4 @@
 #  valgrind needs --trace-children=yes to see through the libtool
 #  wrapper in-tree, and thus gets cluttered with unrelated errors
 #  from shellscripts.
-CFLAGS="-O0" ./configure --enable-developer --prefix=/tmp/_gdnsd_inst && make clean all check && make install && TEST_RUNNER="valgrind --leak-check=full --suppressions=$PWD/docs/gdnsd.supp" make installcheck && grep "ERROR SUM" t/testout/*/gdnsd.out | grep -v ' 0 errors' || rm -rf /tmp/_gdnsd_inst
+CFLAGS="-O0" ./configure --enable-developer --prefix=/tmp/_gdnsd_inst && make clean all check && make install && TEST_RUNNER="valgrind --leak-check=full --suppressions=$PWD/qa/gdnsd.supp" make installcheck && grep "ERROR SUM" t/testout/*/gdnsd.out | grep -v ' 0 errors' || rm -rf /tmp/_gdnsd_inst
