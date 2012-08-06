7df06ae554fa22fd4fd080ebb13e6c4f1803dd42
Brandon Black <blblack@gmail.com>
2012-08-06 12:03:29 -0400

    Basic zsrc_rfc1035 reload code
    This refactors zsrc_rfc1035 into approximately the right shape
    for automatic runtime zone data reloading.  It does seem
    to accomplish runtime reloads (with a 30s interval on scanning
    the zones directory, and a 5s quiesence period (repeating as
    necessaru) for lstat() changes to settle before reloading).
    
    Inotify isn't yet included, just the basic OS-neutral "scandir
    every X seconds" method.  No locking either, but that goes
    in zlist not zsrc.

--- gdnsd/zsrc_rfc1035.c
+++ gdnsd/zsrc_rfc1035.c
@@ -22,27 +22,215 @@
 #include "zscan_rfc1035.h"
 
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include <dirent.h>
 
 static const char RFC1035_DIR[] = "etc/zones/";
 
-// list of all zone_t's we've created from files.
-//   no check for duplicates or indexing, this
-//   is just for cleanup at the end...
-zone_t** zflist = NULL;
-unsigned zflist_count = 0;
-unsigned zflist_alloc = 0;
+// POSIX states that inode+dev uniquely identifies a file on
+//   a given system.  Therefore those + mtime should uniquely
+//   identify a set of file contents for a given pathname over
+//   time through ops like create/destroy/modify/rename/remount/etc...
+// The special value of all members being zero indicates a
+//   non-existent (e.g. deleted) file.  The same value is used
+//   to indicate an invalid zonefile (e.g. the pathname is
+//   a subdirectory, a socket, a softlink, etc...)
+typedef struct {
+    time_t m; // st.st_mtime
+    ino_t i;  // st.st_inode
+    dev_t d;  // st.st_dev
+} statcmp_t;
 
-static void zflist_add(zone_t* z) {
-    if(unlikely(zflist_count == zflist_alloc)) {
-        if(zflist_alloc)
-            zflist_alloc *= 2;
-        else
-            zflist_alloc = 8;
-        zflist = realloc(zflist, zflist_alloc * sizeof(zone_t*));
+static bool statcmp_eq(statcmp_t* a, statcmp_t* b) {
+    return !((a->m ^ b->m) | (a->i ^ b->i) | (a->d ^ b->d));
+}
+
+// check for 0/0/0, indicating deleted or invalid (e.g. socket)
+static bool statcmp_nx(statcmp_t* a) {
+    return !(a->m | a->i | a->d);
+}
+
+// represents a zone file
+// on initial load, update_pending is false, and thus "pending" is irrelevant
+// when change detection sees a statcmp diff between "loaded" and the
+//   filesystem, it's going to set "update_pending" and save the fs info
+//   to "pending", while in many cases a quiescence timer waits for further
+//   updates.
+// when "pending" and the raw FS have stabilized, then the zone is actually
+//   reloaded and "loaded" is set to "pending" values and the update_pending
+//   flag is cleared.
+typedef struct {
+    unsigned hash;       // hash of "fn"
+    unsigned generation; // generation counter for deletion checks
+    char* full_fn;       // "etc/zones/example.com"
+    const char* fn;      // ptr to "example.com" in above storage
+    zone_t* zone;        // zone data
+    ev_timer* pending_event; // pending quiescence timer, NULL if no pending change
+    statcmp_t pending;   // lstat() info on pending update
+    statcmp_t loaded;    // lstat() info on loaded data
+} zfile_t;
+
+// hash of all extant zonefiles
+static zfile_t** zfhash = NULL;
+static unsigned zfhash_count = 0;
+static unsigned zfhash_alloc = 0;
+static unsigned generation = 0; // deletion checks use this...
+
+// ZFILE_DELETED is a deleted hash entry that can be reclaimed
+static void* const ZFILE_DELETED = (void*)(uintptr_t)0x1;
+// SLOT_REAL means not NULL and also not a reclaimable deleted entry
+#define SLOT_REAL(x) ((uintptr_t)x & ~1UL)
+
+// XXX this should move over to zlist...
+static void zone_delete(zone_t* zone) {
+    dmn_assert(zone);
+    if(zone->root)
+        ltree_destroy(zone->root);
+    lta_destroy(zone->arena);
+    free(zone->src);
+    free(zone);
+}
+
+F_NONNULL
+static void zf_delete(zfile_t* zf) {
+    dmn_assert(zf);
+    if(zf->zone)
+        zone_delete(zf->zone);
+    if(zf->full_fn)
+        free(zf->full_fn);
+    if(zf->pending_event)
+        free(zf->pending_event);
+    free(zf);
+}
+
+static void statcmp_set(const char* full_fn, statcmp_t* out) {
+    dmn_assert(full_fn); dmn_assert(out);
+
+    struct stat st;
+    int lstat_rv = lstat(full_fn, &st);
+    if(likely(!lstat_rv && S_ISREG(st.st_mode))) {
+        out->m = st.st_mtime;
+        out->i = st.st_ino;
+        out->d = st.st_dev;
+    }
+    else {
+        out->m = 0;
+        out->i = 0;
+        out->d = 0;
+    }
+}
+
+// probably a poor choice, just wanted something quick for testing XXX
+F_NONNULL
+static unsigned str_djb_hash(const char* input) {
+   dmn_assert(input);
+
+   unsigned hash = 5381;
+   char c;
+   while((c = *input++))
+       hash = (hash * 33) ^ c;
+
+   return hash;
+}
+
+// grow hash by doubling, while also
+//   clearing out deletion placeholders
+F_NONNULL
+static void zfhash_grow(void) {
+    if(unlikely(!zfhash_alloc)) {
+        // initial call on empty hash
+        dmn_assert(!zfhash);
+        dmn_assert(!zfhash_count);
+        zfhash_alloc = 16;
+        zfhash = calloc(16, sizeof(zfile_t*));
+        return;
     }
 
-    zflist[zflist_count++] = z;
+    const unsigned new_alloc = zfhash_alloc << 1; // double
+    const unsigned new_hash_mask = new_alloc - 1;
+    zfile_t** new_hash = calloc(new_alloc, sizeof(zfile_t*));
+
+    for(unsigned i = 0; i < zfhash_alloc; i++) {
+        zfile_t* zf = zfhash[i];
+        if(SLOT_REAL(zf)) {
+            unsigned jmpby = 1;
+            unsigned slot = zf->hash & new_hash_mask;
+            while(new_hash[slot]) {
+                slot += jmpby++;
+                slot &= new_hash_mask;
+            }
+            new_hash[slot] = zf;
+        }
+    }
+
+    free(zfhash);
+    zfhash = new_hash;
+    zfhash_alloc = new_alloc;
+}
+
+// assumes this filename does not exist in hash already,
+// called must use zfhash_find() first!
+F_NONNULL
+static void zfhash_add(zfile_t* zf) {
+    dmn_assert(zf);
+    dmn_assert(zf->fn);
+    dmn_assert(zf->full_fn);
+
+    // Max 25% load
+    if(unlikely(zfhash_count >= (zfhash_alloc >> 2)))
+        zfhash_grow();
+
+    const unsigned hash_mask = zfhash_alloc - 1;
+    unsigned slot = zf->hash & hash_mask;
+    unsigned jmpby = 1;
+    while(SLOT_REAL(zfhash[slot])) {
+        slot += jmpby++;
+        slot &= hash_mask;
+    }
+    zfhash[slot] = zf;
+    zfhash_count++;
+}
+
+F_NONNULL
+static void zfhash_del(zfile_t* zf) {
+    dmn_assert(zf);
+    dmn_assert(zf->fn);
+    dmn_assert(zf->full_fn);
+
+    const unsigned hash_mask = zfhash_alloc - 1;
+    unsigned slot = zf->hash & hash_mask;
+    unsigned jmpby = 1;
+    while(zfhash[slot] != zf) {
+        slot += jmpby++;
+        slot &= hash_mask;
+    }
+
+    zfhash[slot] = ZFILE_DELETED;
+    zfhash_count--;
+
+    zf_delete(zf);
+}
+
+F_NONNULL
+static zfile_t* zfhash_find(const char* zfn) {
+    dmn_assert(zfn);
+
+    if(likely(zfhash_alloc)) {
+        const unsigned zfn_hash = str_djb_hash(zfn);
+        const unsigned hash_mask = zfhash_alloc - 1;
+        unsigned slot = zfn_hash & hash_mask;
+        unsigned jmpby = 1;
+        zfile_t* cand;
+        while((cand = zfhash[slot])) {
+            if(SLOT_REAL(cand) && cand->hash == zfn_hash && !strcmp(cand->fn, zfn))
+                return cand;
+            slot += jmpby++;
+            slot &= hash_mask;
+        }
+    }
+
+    return NULL;
 }
 
 F_NONNULL
@@ -99,88 +287,207 @@ static const uint8_t* make_zone_dname(const char* zf_name, ltarena_t* arena) {
     return rv;
 }
 
-static bool process_zonefile(const char* zfn) {
-    dmn_assert(zfn);
+F_NONNULL
+static zone_t* zone_from_zf(zfile_t* zf) {
+    dmn_assert(zf);
     zone_t* z = calloc(1, sizeof(zone_t));
-    z->src = str_combine(RFC1035_DIR, zfn, NULL); // XXX not quite right
-    // XXX missing mtime, too
+    z->src = str_combine("rfc1035:", zf->fn, NULL);
     z->arena = lta_new();
-    z->dname = make_zone_dname(zfn, z->arena);
+    z->dname = make_zone_dname(zf->fn, z->arena);
     if(!z->dname) {
-        lta_destroy(z->arena);
-        free(z->src);
-        free(z);
-        return true;
+        zone_delete(z);
+        return NULL;
     }
 
     z->hash = dname_hash(z->dname);
     ltree_init_zone(z);
-
-    if(unlikely(scan_zone(z, z->src))) {
-        lta_destroy(z->arena);
-        free(z->src);
-        free(z);
-        return true;
+    if(unlikely(scan_zone(z, zf->full_fn))) {
+        zone_delete(z);
+        return NULL;
     }
 
     lta_close(z->arena);
     if(unlikely(ltree_postproc_zone(z))) {
-        lta_destroy(z->arena);
-        free(z->src);
-        free(z);
-        return true;
-    }
-
-    zlist_update(NULL, z);
-    zflist_add(z);
-    return false;
-}
-
-static void zone_delete(zone_t* zone) {
-    dmn_assert(zone);
-    if(zone->root)
-        ltree_destroy(zone->root);
-    lta_destroy(zone->arena);
-    free(zone->src);
-    free(zone);
-}
-
-static void zsrc_rfc1035_unload_zones(void) {
-    for(unsigned i = 0; i < zflist_count; i++) {
-        zone_t* z = zflist[i];
-        zlist_update(z, NULL);
         zone_delete(z);
+        return NULL;
     }
+
+    return z;
 }
 
-// XXX in the future when this merges with the non-inotify
-//   reload scanner, we don't want directory errors to be fatal
-void zsrc_rfc1035_load_zones(void) {
-    DIR* zdhandle = opendir(RFC1035_DIR);
-    if(!zdhandle)
-        log_fatal("Cannot open zones directory '%s': %s", RFC1035_DIR, dmn_strerror(errno));
+F_NONNULL
+static void quiesce_check(struct ev_loop* reload_loop, ev_timer* timer, int revents) {
+    dmn_assert(reload_loop);
+    dmn_assert(timer);
+    dmn_assert(revents = EV_TIMER);
 
-    unsigned success = 0;
-    unsigned failure = 0;
-    struct dirent* zfdi;
-    while((zfdi = readdir(zdhandle))) {
-        if(likely(zfdi->d_name[0] != '.')) {
-            if(process_zonefile(zfdi->d_name))
-                failure++;
-            else
-                success++;
+    zfile_t* zf = (zfile_t*)timer->data;
+    dmn_assert(zf->pending_event == timer);
+
+    statcmp_t newstat;
+    statcmp_set(zf->full_fn, &newstat);
+    if(statcmp_eq(&newstat, &zf->pending)) {
+        if(statcmp_nx(&newstat)) {
+            if(zf->zone) {
+                dmn_assert(!statcmp_nx(&zf->loaded));
+                zlist_update(zf->zone, NULL);
+            }
+            zfhash_del(zf);
+        }
+        else { // quiesced state isn't deleted, we need to load data
+            zone_t* z = zone_from_zf(zf);
+            // re-check that file didn't change while loading
+            statcmp_t post_check;
+            statcmp_set(zf->full_fn, &post_check);
+            if(!statcmp_eq(&zf->pending, &post_check)) {
+                if(z)
+                     zone_delete(z);
+                ev_timer_set(timer, 5., 0.); // XXX needs to match old timer, or something...?
+                ev_timer_start(reload_loop, timer);
+            }
+            else {
+                if(z) {
+                    memcpy(&zf->loaded, &zf->pending, sizeof(statcmp_t));
+                    z->mtime = zf->loaded.m;
+                    zlist_update(zf->zone, z);
+                    if(zf->zone)
+                        zone_delete(zf->zone);
+                    zf->zone = z;
+                }
+                free(zf->pending_event);
+                zf->pending_event = NULL;
+            }
         }
     }
+    else {
+        ev_timer_set(timer, 5., 0.); // XXX needs to match old timer, or something...?
+        ev_timer_start(reload_loop, timer);
+    }
+}
 
-    if(closedir(zdhandle))
-        log_fatal("closedir(%s) failed: %s", RFC1035_DIR, dmn_strerror(errno));
+F_NONNULL
+static void process_zonefile(const char* zfn, struct ev_loop* reload_loop, const double quiesce_time) {
+    dmn_assert(zfn);
+    dmn_assert(reload_loop);
 
-    log_info("%u zones loaded successfully (%u failed)", success, failure);
+    const char* fn;
+    char* full_fn = str_combine(RFC1035_DIR, zfn, &fn);
 
-    if(atexit(zsrc_rfc1035_unload_zones))
-        log_fatal("atexit(zsrc_rfc1035_unload_zones) failed: %s", logf_errno());
+    statcmp_t newstat;
+    statcmp_set(full_fn, &newstat);
+    zfile_t* current_zft = zfhash_find(fn);
+
+    if(!statcmp_nx(&newstat) && !current_zft) {
+        current_zft = calloc(1, sizeof(zfile_t));
+        current_zft->full_fn = full_fn;
+        current_zft->fn = fn;
+        current_zft->hash = str_djb_hash(fn);
+        zfhash_add(current_zft);
+    }
+    else {
+        free(full_fn);
+    }
+
+    if(current_zft) {
+        current_zft->generation = generation;
+        if(current_zft->pending_event) { // already had a change queued...
+            if(!statcmp_eq(&newstat, &current_zft->pending)) { // but it changed again!
+                memcpy(&current_zft->pending, &newstat, sizeof(statcmp_t));
+                ev_timer_stop(reload_loop, current_zft->pending_event);
+                ev_timer_set(current_zft->pending_event, quiesce_time, 0.); // XXX timer params config
+                ev_timer_start(reload_loop, current_zft->pending_event);
+            }
+            // else (if pending state has not changed) let timer continue as it was...
+        }
+        else if(!statcmp_eq(&newstat, &current_zft->pending)) { // initial change detected
+            memcpy(&current_zft->pending, &newstat, sizeof(statcmp_t));
+            current_zft->pending_event = malloc(sizeof(ev_timer));
+            ev_timer_init(current_zft->pending_event, quiesce_check, quiesce_time, 0.); // XXX timer params config
+            current_zft->pending_event->data = current_zft;
+            ev_timer_start(reload_loop, current_zft->pending_event);
+        }
+    }
+}
+
+static void unload_zones(void) {
+    for(unsigned i = 0; i < zfhash_alloc; i++) {
+        zfile_t* zf = zfhash[i];
+        if(SLOT_REAL(zf)) {
+            zlist_update(zf->zone, NULL);
+            zf_delete(zf);
+        }
+    }
+}
+
+static void scan_dir(struct ev_loop* reload_loop, double quiesce_time) {
+    DIR* zdhandle = opendir(RFC1035_DIR);
+    if(!zdhandle) {
+        log_err("Cannot open zones directory '%s': %s", RFC1035_DIR, dmn_strerror(errno));
+    }
+    else {
+        struct dirent* zfdi;
+        while((zfdi = readdir(zdhandle)))
+            if(likely(zfdi->d_name[0] != '.'))
+                process_zonefile(zfdi->d_name, reload_loop, quiesce_time);
+        if(closedir(zdhandle))
+            log_err("closedir(%s) failed: %s", RFC1035_DIR, dmn_strerror(errno));
+    }
+}
+
+// This is the complement to the periodic scandir(), which
+//  detects deletion events.  Its job is to run immediately
+//  after the scandir loop and find zfhash entries that lack
+//  the current "generation" counter value, indicating they
+//  were not seen during scandir(), and feed them back into
+//  process_zonefile() to be picked up as deletions.
+F_NONNULL
+static void check_missing(struct ev_loop* reload_loop) {
+    dmn_assert(reload_loop);
+    dmn_assert(generation);
+
+    for(unsigned i = 0; i < zfhash_alloc; i++) {
+        zfile_t* zf = zfhash[i];
+        if(SLOT_REAL(zf)) {
+            if(zf->generation != generation) {
+                process_zonefile(zf->fn, reload_loop, 5.0); // XXX timeout config again
+            }
+        }
+    }
+}
+
+F_NONNULL
+static void periodic_scan(struct ev_loop* reload_loop, ev_timer* rtimer, int revents) {
+    dmn_assert(reload_loop);
+    dmn_assert(rtimer);
+    dmn_assert(revents == EV_TIMER);
+
+    generation++;
+    scan_dir(reload_loop, 5.);
+    check_missing(reload_loop);
+}
+
+// ev stuff
+static ev_timer* reload_timer = NULL;
+
+/*************************/
+/*** Public interfaces ***/
+/*************************/
+
+void zsrc_rfc1035_load_zones(void) {
+    struct ev_loop* temp_load_loop = ev_loop_new(EVFLAG_AUTO);
+    scan_dir(temp_load_loop, 0.);
+    ev_run(temp_load_loop, 0);
+    ev_loop_destroy(temp_load_loop);
+    free(reload_timer);
+    if(atexit(unload_zones))
+        log_fatal("atexit(unload_zones) failed: %s", logf_errno());
 }
 
 void zsrc_rfc1035_runtime_init(struct ev_loop* zdata_loop) {
     dmn_assert(zdata_loop);
+
+    // XXX "30" should be configurable
+    reload_timer = calloc(1, sizeof(ev_timer));
+    ev_timer_init(reload_timer, periodic_scan, 30., 30.);
+    ev_timer_start(zdata_loop, reload_timer);
 }
