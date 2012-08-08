5dd59d93fb8709a431e06ce1d2f10156e5c05ed4
Brandon Black <blblack@gmail.com>
2012-08-08 00:39:34 -0400

    shutdown zone leaks only checked in debug

--- gdnsd/zlist.c
+++ gdnsd/zlist.c
@@ -209,7 +209,7 @@ static void zlist_grow(void) {
 static void zlist_destroy(void) {
     dmn_assert(zlist);
     dmn_assert(zlist_alloc);
-    if(1) { // XXX debug only or something?
+    if(dmn_debug()) {
         for(unsigned i = 0; i < zlist_alloc; i++) {
             zone_t* z = zlist[i];
             if(SLOT_REAL(z))
