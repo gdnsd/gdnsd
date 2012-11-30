12d8aa8a6eb1eee258d7950cda72ebaeb3c8b011
Brandon Black <blblack@gmail.com>
2012-11-30 06:13:46 +0000

    gdmaps unit tests: valground, and non-geoip ones now run w/o data

--- plugins/meta/libgdmaps/t/trunner.sh
+++ plugins/meta/libgdmaps/t/trunner.sh
@@ -3,32 +3,45 @@
 TODIR=$ABDIR/testroot
 mkdir -p $TODIR/etc/geoip >/dev/null 2>&1
 
-skip_all=0
+skip_geoip=0
 for gdata in $GEOLITE_FILES; do
     if [ ! -f $ABDIR/$gdata ]; then
-        skip_all=1
+        skip_geoip=1
+    else
+        cp $ABDIR/$gdata $TODIR/etc/geoip/
     fi
-    cp $ABDIR/$gdata $TODIR/etc/geoip/
 done
 
-if [ $skip_all -eq 1 ]; then
-    echo "Skipping developer libgdmaps unit tests; missing GeoLite data."
-    exit 0
+if [ $skip_geoip -eq 1 ]; then
+    echo "Skipping GeoIP-based libgdmaps unit tests; missing GeoLite data."
 fi
 
 for netsfile in $ASDIR/*.nets; do
     cp $netsfile $TODIR/etc/geoip/
 done
 
+# Note: TEST_RUNNER support here was added primarily
+#  for qa/valgrind.sh.  It will require "make install"
+#  to some temp install dir in order for the unit tests
+#  to find libgdnsd.so
+
 for tnam in $TLIST; do
-    echo "Running test $tnam ..."
-    cp $ASDIR/$tnam.cfg $TODIR/etc/config >/dev/null 2>&1
-    $ABDIR/$tnam.bin $TODIR >$TODIR/output 2>&1
-    rv=$?
-    if [ $rv -ne 0 ]; then
-        echo "Test $tnam failed w/ exit status $rv; Test Output:"
-        cat $TODIR/output
-        exit 99
+    grep -q geoip_db $ASDIR/$tnam.cfg
+    if [ $? -eq 1 -o $skip_geoip -eq 0 ]; then
+        echo "Running test $tnam ..."
+        TOFILE=$TODIR/$tnam.out
+        cp $ASDIR/$tnam.cfg $TODIR/etc/config >/dev/null 2>&1
+        if [ "x$TEST_RUNNER" != "x" ]; then
+            $TEST_RUNNER $ABDIR/.libs/$tnam.bin $TODIR >$TOFILE 2>&1
+        else
+            $ABDIR/$tnam.bin $TODIR >$TOFILE 2>&1
+        fi
+        rv=$?
+        if [ $rv -ne 0 ]; then
+            echo "Test $tnam failed w/ exit status $rv; Test Output:"
+            cat $TOFILE
+            exit 99
+        fi
     fi
 done
 
--- qa/valgrind.sh
+++ qa/valgrind.sh
@@ -9,9 +9,10 @@ set -e
 CFLAGS="-O0" ./configure --enable-developer --prefix=/tmp/_gdnsd_inst --with-def-rootdir=/tmp/_gdnsd_inst/root
 make clean all
 make install
+TEST_RUNNER="valgrind --leak-check=full --suppressions=$PWD/qa/gdnsd.supp" make -C plugins/meta/libgdmaps/t check
 TEST_RUNNER="valgrind --leak-check=full --suppressions=$PWD/qa/gdnsd.supp" make installcheck
 set +e
 set +x
-grep "ERROR SUM" t/testout/*/gdnsd.out \
+grep "ERROR SUM" t/testout/*/gdnsd.out plugins/meta/libgdmaps/t/testroot/*.out \
   | grep -v ' 0 errors' \
     || rm -rf /tmp/_gdnsd_inst
