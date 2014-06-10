gdnsd/gdnsd

commit ae603f033c9aa2b55bcacc61e1d32da291088a3b

Remove chroot() from gdnsd
Direct support for chroot() seems outmoded by more modern
and complete container solutions that would be configured
and setup outside of the daemon code itself.  Removing
direct chroot() support kills a fair amount of code/docs
complexity and frees us from having to make any more excuses
about why it doesn't quite work right out of the box under
certain conditions or system configurations.

From the perspective of a user who wasn't using chroot()
anyways, the primary visible change is the replacement
of the "-d rootdir" commandline argument with "-c configfile"

gdnsd/conf.c
@@ -23,6 +23,7 @@
 #include "gdnsd/misc.h"
 #include "gdnsd/log.h"
 #include "gdnsd/paths.h"
+#include "gdnsd/paths-priv.h"
 #include "gdnsd/plugapi-priv.h"
 #include "gdnsd/mon-priv.h"
 
@@ -464,34 +465,33 @@ static void process_listen(const vscf_data_t* listen_opt, const dns_addr_t* addr
     dmn_assert(tnum == gconfig.num_dns_threads);
 }
 
-static const vscf_data_t* conf_load_vscf(void) {
+static const vscf_data_t* conf_load_vscf(const char* cfg_file) {
     const vscf_data_t* out = NULL;
 
-    char* cfg_path = gdnsd_resolve_path_cfg("config", NULL);
-
     struct stat cfg_stat;
-    if(!stat(cfg_path, &cfg_stat)) {
-        log_info("Loading configuration from '%s'", logf_pathname(cfg_path));
+    if(!stat(cfg_file, &cfg_stat)) {
+        log_info("Loading configuration from '%s'", cfg_file);
         char* vscf_err;
-        out = vscf_scan_filename(cfg_path, &vscf_err);
+        out = vscf_scan_filename(cfg_file, &vscf_err);
         if(!out)
-            log_fatal("Loading configuration from '%s' failed: %s", logf_pathname(cfg_path), vscf_err);
+            log_fatal("Loading configuration from '%s' failed: %s", cfg_file, vscf_err);
         if(!vscf_is_hash(out)) {
             dmn_assert(vscf_is_array(out));
-            log_fatal("Config file '%s' cannot be an '[ array ]' at the top level", logf_pathname(cfg_path));
+            log_fatal("Config file '%s' cannot be an '[ array ]' at the top level", cfg_file);
         }
     }
     else {
-        log_info("No config file at '%s', using defaults", logf_pathname(cfg_path));
+        log_info("No config file at '%s', using defaults", cfg_file);
     }
 
-    free(cfg_path);
     return out;
 }
 
-void conf_load(const bool force_zss, const bool force_zsd) {
+void conf_load(const char* cfg_file, const bool force_zss, const bool force_zsd, const bool dirs_only) {
 
-    const vscf_data_t* cfg_root = conf_load_vscf();
+    if(!cfg_file)
+        cfg_file = gdnsd_get_default_config_file();
+    const vscf_data_t* cfg_root = conf_load_vscf(cfg_file);
 
 #ifndef NDEBUG
     // in developer debug builds, exercise clone+destroy
@@ -505,6 +505,30 @@ void conf_load(const bool force_zss, const bool force_zsd) {
     dmn_assert(!cfg_root || vscf_is_hash(cfg_root));
 
     const vscf_data_t* options = cfg_root ? vscf_hash_get_data_byconstkey(cfg_root, "options", true) : NULL;
+
+    // daemon actions only need the rundir, so we process dirs first and bail
+    //   early in those cases without doing the rest of the complex stuff
+    {
+        const char* cfg_run_dir = NULL;
+        const char* cfg_state_dir = NULL;
+        const char* cfg_config_dir = NULL;
+        if(options) {
+            if(!vscf_is_hash(options))
+                log_fatal("Config key 'options': wrong type (must be hash)");
+            CFG_OPT_STR_NOCOPY(options, run_dir, cfg_run_dir);
+            CFG_OPT_STR_NOCOPY(options, state_dir, cfg_state_dir);
+            CFG_OPT_STR_NOCOPY(options, config_dir, cfg_config_dir);
+        }
+
+        gdnsd_set_dirs(cfg_run_dir, cfg_state_dir, cfg_config_dir, cfg_file);
+    }
+
+    // fast-path exit for dirs_only case
+    if(dirs_only) {
+        if(cfg_root)
+            vscf_destroy(cfg_root);
+        return;
+    }
 
     const vscf_data_t* listen_opt = NULL;
     const vscf_data_t* http_listen_opt = NULL;
@@ -525,9 +549,6 @@ void conf_load(const bool force_zss, const bool force_zsd) {
     };
 
     if(options) {
-        if(!vscf_is_hash(options))
-            log_fatal("Config key 'options': wrong type (must be hash)");
-
         CFG_OPT_INT(options, priority, -20L, 20L);
         CFG_OPT_BOOL(options, include_optional_ns);
         CFG_OPT_BOOL(options, realtime_stats);

gdnsd/libgdnsd/gdnsd/paths-priv.h
@@ -23,31 +23,16 @@
 #include "gdnsd/compiler.h"
 #include "gdnsd/paths.h"
 
-// Called by core daemon only, once at startup.
-//   It cleans up "rootdir_in" via realpath(), verifies
-//   existence, and does a chdir() into it.
-// From here forward, the data root dir paths
-//   are used as relative paths, e.g. "etc/config",
-//   "etc/geoip/GeoIPRegion.dat", etc...
-// (Unless the default or argument is "system", in
-//   which case we set up for unrooted execution
-//   with system default paths from autoconf)
-void gdnsd_set_rootdir(const char* rootdir_in);
+// Return the compiled-in default config file pathname
+const char* gdnsd_get_default_config_file(void);
 
-// Returns the realpath()-cleaned actual rootdir
-//   determined and used above.  Almost none of the
-//   code should actually need this, except the
-//   security code for chroot().
-F_PURE
-const char* gdnsd_get_rootdir(void);
-
-// this returns the compiled default (a path
-//   for chroot default, or "system", never NULL),
-//   to help with the usage() output...
-F_PURE
-const char* gdnsd_get_def_rootdir(void);
-
-// special-case, get the rundir as libdmn wants to see it
-const char* gdnsd_get_rundir_for_dmn(void);
+// Set any explicitly-configured directories to non-default
+//   values.  Only supply explicit overrides!
+// The state/run dirs will get compiled-in defaults if NULL.
+// The internal config dir behavior has 3 basic cases:
+//   1) if config_dir, use that.
+//   2) else if config_file, use dirname(config_file)
+//   3) else use dirname(gdnsd_get_default_config_file())
+void gdnsd_set_dirs(const char* run_dir, const char* state_dir, const char* config_dir, const char* config_file);
 
 #endif // GDNSD_PATHS_PRIV_H

gdnsd/libgdnsd/paths.c
@@ -24,8 +24,6 @@
 #include "gdnsd/misc.h"
 #include "gdnsd/log.h"
 
-#include "cfg-dirs.h"
-
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -40,128 +38,93 @@
 #include <fcntl.h>
 #include <pthread.h>
 
-/* misc */
+#include "cfg-dirs.h"
 
-// this will be "system" or an absolute directory
-static const char* const def_rootdir = GDNSD_DEF_ROOTDIR;
+/* paths */
 
-// "system" (or default unrooted) leaves this as NULL
-static const char* rootdir = NULL;
+const char* gdnsd_get_default_config_file(void) { return GDNSD_DEF_CONFIG; }
 
-// readonly private interfaces to core code
-const char* gdnsd_get_rootdir(void) { return rootdir; }
-const char* gdnsd_get_def_rootdir(void) { return def_rootdir; }
-
-// this is for use after basic rootdir setting and chdir...
-static void ensure_dir(const char* dpath) {
+F_NONNULL
+static char* gdnsd_realdir(const char* dpath, const char* desc, const bool create, mode_t def_mode) {
     struct stat st;
-    if(lstat(dpath, &st)) {
-        if(mkdir(dpath, 0755))
-            log_fatal("mkdir(%s) failed: %s", logf_pathname(dpath), dmn_logf_strerror(errno));
-        log_info("Created directory %s", logf_pathname(dpath));
+    if(stat(dpath, &st)) {
+        if(create) {
+            if(mkdir(dpath, def_mode))
+                log_fatal("mkdir of %s directory '%s' failed: %s", desc, dpath, dmn_logf_strerror(errno));
+            log_info("Created %s directory %s", desc, dpath);
+        }
+        else {
+            log_fatal("%s directory %s does not exist!", desc, dpath);
+        }
     }
     else if(!S_ISDIR(st.st_mode)) {
-        log_fatal("'%s' is not a directory (but should be)!", logf_pathname(dpath));
+        log_fatal("%s directory '%s' is not a directory (but should be)!", desc, dpath);
     }
-}
 
-// as above for the initial rootdir check itself, note the use
-//   of stat() rather than lstat() (so that a symlink to a directory
-//   will work (which realpath will clean up afterwards)).
-static void ensure_rootdir(const char* rpath) {
-    struct stat st;
-    if(stat(rpath, &st)) {
-        if(mkdir(rpath, 0755))
-            log_fatal("mkdir(%s) failed: %s", rpath, dmn_logf_strerror(errno));
-    }
-    else if(!S_ISDIR(st.st_mode)) {
-        log_fatal("'%s' is not a directory (but should be)!", rpath);
-    }
-}
-
-char* gdnsd_realpath(const char* path_in, const char* desc) {
-    char* out = realpath(path_in, NULL);
+    char* out = realpath(dpath, NULL);
     if(!out)
-        log_fatal("Cleanup/validation of %s pathname '%s' failed: %s",
-            desc, path_in, dmn_logf_strerror(errno));
-    if(strcmp(path_in, out))
-        log_info("%s path '%s' cleaned up as '%s'", desc, path_in, out);
+        log_fatal("Validation of %s directory '%s' failed: %s",
+            desc, dpath, dmn_logf_strerror(errno));
+    if(strcmp(dpath, out))
+        log_info("%s directory '%s' cleaned up as '%s'", desc, dpath, out);
     return out;
 }
 
-// basic path types
 typedef enum {
-    CFG   = 0,
-    RUN   = 1,
-    STATE = 2,
+    RUN     = 0,
+    STATE   = 1,
+    CFG     = 2,
+    LIBEXEC = 3,
 } path_typ_t;
 
-// first index is boolean rootdir
-static const char* path_base[2][3] = {
-    [0] = {
-        [CFG]   = GDNSD_ETCDIR,
-        [RUN]   = GDNSD_RUNDIR,
-        [STATE] = GDNSD_STATEDIR,
-    },
-    [1] = {
-        [CFG]   = "etc/",
-        [RUN]   = "run/",
-        [STATE] = "var/",
-    },
-};
+static const char* gdnsd_dirs[4] = { NULL, NULL, NULL, NULL };
 
-void gdnsd_set_rootdir(const char* rootdir_in) {
-    dmn_assert(!rootdir);
-    dmn_assert(def_rootdir);
+void gdnsd_set_dirs(const char* run_dir, const char* state_dir, const char* config_dir, const char* config_file) {
+    if(!run_dir)
+        run_dir = strdup(GDNSD_RUNDIR);
+    gdnsd_dirs[RUN] = gdnsd_realdir(run_dir, "run", true, 0750);
 
-    const char* rootdir_setting
-        = rootdir_in ? rootdir_in : def_rootdir;
+    if(!state_dir)
+        state_dir = strdup(GDNSD_STATEDIR);
+    gdnsd_dirs[STATE] = gdnsd_realdir(state_dir, "state", true, 0755);
 
-    dmn_assert(rootdir_setting);
-
-    // libdmn takes care of the mkdir/perms on the rundir for the pidfile,
-    //   which is why there's no ensure_dir() for it below
-    if(!strcmp(rootdir_setting, "system")) {
-        // Not using a root directory, using system paths
-        if(chdir("/"))
-            log_fatal("Failed to chdir('/'): %s", dmn_logf_strerror(errno));
-        ensure_dir(path_base[0][STATE]);
+    if(!config_dir) {
+        if(!config_file)
+            config_file = strdup(GDNSD_DEF_CONFIG);
+        char* cfg_file_copy = strdup(config_file);
+        gdnsd_dirs[CFG] = gdnsd_realdir(dirname(cfg_file_copy), "config", false, 0);
+        free(cfg_file_copy);
     }
     else {
-        // Using a root directory:
-        // realpath() wants an extant file to reference,
-        //  so we have to do our stat/mkdir on the original first
-        ensure_rootdir(rootdir_setting);
-        rootdir = gdnsd_realpath(rootdir_setting, "data root");
-        if(chdir(rootdir))
-            log_fatal("Failed to chdir('%s'): %s", rootdir, dmn_logf_strerror(errno));
-
-        // build basic/common directory structure if missing
-        ensure_dir("etc/");
-        ensure_dir("etc/zones");
-        ensure_dir("etc/djbdns");
-        ensure_dir("etc/geoip");
-        ensure_dir(path_base[1][STATE]);
+        gdnsd_dirs[CFG] = gdnsd_realdir(config_dir, "config", false, 0);
     }
+
+    // This is just fixed at compiletime, period
+    gdnsd_dirs[LIBEXEC] = strdup(GDNSD_LIBEXECDIR);
 }
 
 static char* gdnsd_resolve_path(const path_typ_t p, const char* inpath, const char* pfx) {
-    dmn_assert(inpath);
+    dmn_assert(gdnsd_dirs[p]);
 
     char* out = NULL;
 
-    if(inpath[0] == '/')
+    if(inpath && inpath[0] == '/') {
         out = strdup(inpath);
-    else if(pfx)
-        out = gdnsd_str_combine_n(4, path_base[!!rootdir][p], pfx, "/", inpath);
-    else
-        out = gdnsd_str_combine_n(2, path_base[!!rootdir][p], inpath);
+    }
+    else if(pfx) {
+        if(inpath)
+            out = gdnsd_str_combine_n(5, gdnsd_dirs[p], "/", pfx, "/", inpath);
+        else
+            out = gdnsd_str_combine_n(3, gdnsd_dirs[p], "/", pfx);
+    }
+    else {
+        if(inpath)
+            out = gdnsd_str_combine_n(3, gdnsd_dirs[p], "/", inpath);
+        else
+            out = strdup(gdnsd_dirs[p]);
+    }
 
     return out;
-}
-
-char* gdnsd_resolve_path_cfg(const char* inpath, const char* pfx) {
-    return gdnsd_resolve_path(CFG, inpath, pfx);
 }
 
 char* gdnsd_resolve_path_run(const char* inpath, const char* pfx) {
@@ -172,6 +135,10 @@ char* gdnsd_resolve_path_state(const char* inpath, const char* pfx) {
     return gdnsd_resolve_path(STATE, inpath, pfx);
 }
 
-const char* gdnsd_get_rundir_for_dmn(void) {
-    return rootdir ? "/run" : path_base[0][RUN];
+char* gdnsd_resolve_path_cfg(const char* inpath, const char* pfx) {
+    return gdnsd_resolve_path(CFG, inpath, pfx);
+}
+
+char* gdnsd_resolve_path_libexec(const char* inpath, const char* pfx) {
+    return gdnsd_resolve_path(LIBEXEC, inpath, pfx);
 }

plugins/meta/libgdmaps/t/gdmaps_test.c
@@ -38,33 +38,33 @@
 #include <gdnsd/plugapi.h>
 #include <gdnsd/misc.h>
 
-// be evil and use the private interface to set the cfdir,
-//   since this is for test mocking and we're part of the main dist
 #include "gdnsd/paths-priv.h"
 
 #include "gdmaps.h"
 #include "gdmaps_test.h"
 
-static const vscf_data_t* conf_load_vscf(void) {
+static const vscf_data_t* conf_load_vscf(const char* cfg_path) {
     const vscf_data_t* out = NULL;
 
-    char* cfg_path = gdnsd_resolve_path_cfg("config", NULL);
+    gdnsd_set_dirs(NULL, NULL, NULL, cfg_path);
 
     struct stat cfg_stat;
+    if(!cfg_path)
+        cfg_path = gdnsd_get_default_config_file();
+
     if(!stat(cfg_path, &cfg_stat)) {
-        log_info("Loading configuration from '%s'", logf_pathname(cfg_path));
+        log_info("Loading configuration from '%s'", cfg_path);
         char* vscf_err;
         out = vscf_scan_filename(cfg_path, &vscf_err);
         if(!out)
-            log_fatal("Configuration from '%s' failed: %s", logf_pathname(cfg_path), vscf_err);
+            log_fatal("Configuration from '%s' failed: %s", cfg_path, vscf_err);
         if(!vscf_is_hash(out))
-            log_fatal("Configuration from '%s' failed: config was an array!", logf_pathname(cfg_path));
+            log_fatal("Configuration from '%s' failed: config was an array!", cfg_path);
     }
     else {
-        log_info("No config file at '%s', using defaults + zones auto-scan", logf_pathname(cfg_path));
+        log_info("No config file at '%s', using defaults + zones auto-scan", cfg_path);
     }
 
-    free(cfg_path);
     return out;
 }
 
@@ -158,12 +158,11 @@ void gdmaps_test_lookup_check(const unsigned tnum, const gdmaps_t* gdmaps, const
         log_fatal("Subtest %u failed: Wanted scope mask %u, got %u", tnum, scope_cmp, scope);
 }
 
-gdmaps_t* gdmaps_test_init(const char* input_rootdir) {
+gdmaps_t* gdmaps_test_init(const char* input_cfgfile) {
 
     dmn_init1(false, true, true, false, "gdmaps_test");
 
-    gdnsd_set_rootdir(input_rootdir);
-    const vscf_data_t* cfg_root = conf_load_vscf();
+    const vscf_data_t* cfg_root = conf_load_vscf(input_cfgfile);
     const vscf_data_t* maps_cfg = conf_get_maps(cfg_root);
     gdmaps_t* gdmaps = gdmaps_new(maps_cfg);
     vscf_destroy(cfg_root);

commit 224635dcfd0a4234bb4f668b61dadd61b227dcef

Fix libgdmaps testsuite breakage from ae603f03

gdnsd/conf.c
@@ -520,7 +520,7 @@ void conf_load(const char* cfg_file, const bool force_zss, const bool force_zsd,
             CFG_OPT_STR_NOCOPY(options, config_dir, cfg_config_dir);
         }
 
-        gdnsd_set_dirs(cfg_run_dir, cfg_state_dir, cfg_config_dir, cfg_file);
+        gdnsd_set_dirs(cfg_run_dir, cfg_state_dir, cfg_config_dir, cfg_file, true);
     }
 
     // fast-path exit for dirs_only case

gdnsd/libgdnsd/gdnsd/paths-priv.h
@@ -22,6 +22,7 @@
 
 #include "gdnsd/compiler.h"
 #include "gdnsd/paths.h"
+#include <stdbool.h>
 
 // Return the compiled-in default config file pathname
 const char* gdnsd_get_default_config_file(void);
@@ -33,6 +34,9 @@ const char* gdnsd_get_default_config_file(void);
 //   1) if config_dir, use that.
 //   2) else if config_file, use dirname(config_file)
 //   3) else use dirname(gdnsd_get_default_config_file())
-void gdnsd_set_dirs(const char* run_dir, const char* state_dir, const char* config_dir, const char* config_file);
+// if runtime_dirs is false, the state/run dirs will not
+//   be checked for existence or created.  Useful for
+//   uses outside of gdnsd itself (e.g. testsuite binaries)
+void gdnsd_set_dirs(const char* run_dir, const char* state_dir, const char* config_dir, const char* config_file, const bool runtime_dirs);
 
 #endif // GDNSD_PATHS_PRIV_H

gdnsd/libgdnsd/paths.c
@@ -79,18 +79,25 @@ typedef enum {
 
 static const char* gdnsd_dirs[4] = { NULL, NULL, NULL, NULL };
 
-void gdnsd_set_dirs(const char* run_dir, const char* state_dir, const char* config_dir, const char* config_file) {
+void gdnsd_set_dirs(const char* run_dir, const char* state_dir, const char* config_dir, const char* config_file, const bool runtime_dirs) {
     if(!run_dir)
-        run_dir = strdup(GDNSD_RUNDIR);
-    gdnsd_dirs[RUN] = gdnsd_realdir(run_dir, "run", true, 0750);
+        run_dir = GDNSD_RUNDIR;
 
     if(!state_dir)
-        state_dir = strdup(GDNSD_STATEDIR);
-    gdnsd_dirs[STATE] = gdnsd_realdir(state_dir, "state", true, 0755);
+        state_dir = GDNSD_STATEDIR;
+
+    if(runtime_dirs) {
+        gdnsd_dirs[RUN] = gdnsd_realdir(run_dir, "run", true, 0750);
+        gdnsd_dirs[STATE] = gdnsd_realdir(state_dir, "state", true, 0755);
+    }
+    else {
+        gdnsd_dirs[RUN] = strdup(run_dir);
+        gdnsd_dirs[STATE] = strdup(state_dir);
+    }
 
     if(!config_dir) {
         if(!config_file)
-            config_file = strdup(GDNSD_DEF_CONFIG);
+            config_file = GDNSD_DEF_CONFIG;
         char* cfg_file_copy = strdup(config_file);
         gdnsd_dirs[CFG] = gdnsd_realdir(dirname(cfg_file_copy), "config", false, 0);
         free(cfg_file_copy);
@@ -100,7 +107,7 @@ void gdnsd_set_dirs(const char* run_dir, const char* state_dir, const char* conf
     }
 
     // This is just fixed at compiletime, period
-    gdnsd_dirs[LIBEXEC] = strdup(GDNSD_LIBEXECDIR);
+    gdnsd_dirs[LIBEXEC] = GDNSD_LIBEXECDIR;
 }
 
 static char* gdnsd_resolve_path(const path_typ_t p, const char* inpath, const char* pfx) {

plugins/meta/libgdmaps/t/gdmaps_test.c
@@ -46,7 +46,7 @@
 static const vscf_data_t* conf_load_vscf(const char* cfg_path) {
     const vscf_data_t* out = NULL;
 
-    gdnsd_set_dirs(NULL, NULL, NULL, cfg_path);
+    gdnsd_set_dirs(NULL, NULL, NULL, cfg_path, false);
 
     struct stat cfg_stat;
     if(!cfg_path)