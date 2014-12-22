3daf636aefe6eb247fbce89daecd14143b4df424
Brandon L Black <blblack@gmail.com>
2014-12-22 19:19:10 +0000

    Do not anticipate partial sendmmsg()
    As a further refinement on aea98584: according to the syscall docs
    since our sendmmsg() is blocking with no timeout, it should not be
    possible for sendmmsg to indicate <vlen packets sent in a
    non-error case, and therefore we can get rid of the crazy loop
    construct for retrying.
    
    backported from master commit 486bb8d1

--- gdnsd/dnsio_udp.c
+++ gdnsd/dnsio_udp.c
@@ -472,21 +472,13 @@ static void mainloop_mmsg(const unsigned width, const int fd, void* dnsp_ctx, dn
                 }
             }
 
-            struct mmsghdr* dgptr = dgrams;
-            while(pkts > 0) {
-                int mmsg_rv = sendmmsg(fd, dgptr, pkts, 0);
-                if(mmsg_rv < 0) {
-                    stats_own_inc(&stats->udp.sendfail);
-                    int sockerr = 0;
-                    socklen_t sock_len = sizeof(sockerr);
-                    (void)getsockopt(fd, SOL_SOCKET, SO_ERROR, &sockerr, &sock_len);
-                    log_err("UDP sendmmsg() failed: %s", dmn_logf_strerror(sockerr));
-                    break;
-                }
-                unsigned sent = (unsigned)mmsg_rv;
-                dmn_assert(sent <= pkts);
-                dgptr += sent; // skip past the successes
-                pkts -= sent; // drop the count of all successes
+            int mmsg_rv = sendmmsg(fd, dgrams, pkts, 0);
+            if(unlikely(mmsg_rv < 0)) {
+                stats_own_inc(&stats->udp.sendfail);
+                int sockerr = 0;
+                socklen_t sock_len = sizeof(sockerr);
+                (void)getsockopt(fd, SOL_SOCKET, SO_ERROR, &sockerr, &sock_len);
+                log_err("UDP sendmmsg() failed: %s", dmn_logf_strerror(sockerr));
             }
         }
         else {
