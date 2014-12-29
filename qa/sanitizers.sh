e66f8f9efcb51667c33daa84c968a177a42eda46
Brandon L Black <blblack@gmail.com>
2014-12-29 17:55:06 +0000

    add comment about sanitizers + ccache

--- qa/sanitizers.sh
+++ qa/sanitizers.sh
@@ -5,6 +5,11 @@ if [ ! -f $PWD/qa/gdnsd.supp ]; then
    exit 99
 fi
 
+# NOTE: the sanitizer blacklists don't work well with ccache because the cache
+# doesn't know to vary on the contents of the blacklist file.  Given that some
+# distributions enable ccache for all compilers silently by default if it's
+# installed at all, it's wise to "ccache -C" if changing a blacklist file.
+
 set -x
 set -e
 export ASAN_OPTIONS="check_initialization_order=1"
