ec05a6fe194a70e1d252afb338a4c99f029ab198
Brandon L Black <blblack@gmail.com>
2015-12-28 23:47:51 +0000

    travis: trying out s/coveralls/codecov/
    also, reduce builds from 3 to 2: just clang+optimized and
    gcc+coverage, rather than also testing gcc+optimized

--- qa/travis-build.sh
+++ qa/travis-build.sh
@@ -21,16 +21,11 @@ case "$GDNSD_TRAVIS_BUILD" in
         CFLAGS=-O3 ./configure
         make -j$TEST_CPUS check
     ;;
-    coveralls)
-        CFLAGS="-O0 -g -fprofile-arcs -ftest-coverage -fno-omit-frame-pointer" CPPFLAGS="-DDMN_NO_UNREACH_BUILTIN -DDMN_NO_FATAL_COVERAGE -DDMN_COVERTEST_EXIT" ./configure --without-hardening
+    codecov)
+        CFLAGS="-O0 -g -coverage -fno-omit-frame-pointer" CPPFLAGS="-DDMN_NO_UNREACH_BUILTIN -DDMN_NO_FATAL_COVERAGE -DDMN_COVERTEST_EXIT" ./configure --without-hardening
         make -j$TEST_CPUS
-        lcov -c -i -d . -o gdnsd-base.info
         make check
-        lcov -c -d . -o gdnsd-test.info
-        lcov -a gdnsd-base.info -a gdnsd-test.info -o gdnsd-tested.info
-        # This filters out the ragel-generated parsers, the inlines from liburcu, and any test-only sources
-        lcov -o gdnsd-filtered.info -r gdnsd-tested.info zscan_rfc1035.c vscf.c urcu-qsbr.h 't/lib*/*.c'
-        coveralls-lcov gdnsd-filtered.info
+        codecov
     ;;
     *)
         echo "Invalid GDNSD_TRAVIS_BUILD: $GDNSD_TRAVIS_BUILD"
