gdnsd/gdnsd

commit 56b9f14d21098af2f4307565036d621d7856624c

geoip2: compatibility with libmaxminddb 1.1.5+
libmaxminddb broke some data structure assumptions in the minor
version bump from 1.1.4 to 1.1.5.  Their argument is that our code
shouldn't have been looking this deeply at the data structures,
but then again there's no other interface to efficiently iterate
the database yet.  Either way, this seems to paper over the
problem...

Fixes #108

libgdmaps/gdgeoip2.c
@@ -41,6 +41,12 @@
 
 #include <maxminddb.h>
 
+// libmaxminddb broke assumptions for code which manual recurses the data
+// with the bump from 1.1.4 to 1.1.5.  New interfaces to fix this in the
+// long term will appear in 1.2.0, I think.  For now, we set a static flag
+// early in startup to address the discrepancy.
+static bool libmmdb_gt_114 = false;
+
 typedef struct {
     unsigned offset;
     uint32_t dclist;
@@ -206,8 +212,8 @@ static const char* GEOIP2_PATH_CITY[] = { "city", "names", "en", NULL };
         }\
     }\
     else if(mmrv_ != MMDB_LOOKUP_PATH_DOES_NOT_MATCH_DATA_ERROR) {\
-        dmn_log_err("plugin_geoip: map %s: Unexpected error fetching GeoIP2 data (%s)",\
-            state->db->map_name, MMDB_strerror(mmrv_));\
+        dmn_log_err("plugin_geoip: map %s: Unexpected error fetching GeoIP2 data (%i: %s): Backtrace: %s",\
+            state->db->map_name, mmrv_, MMDB_strerror(mmrv_), dmn_logf_bt());\
         siglongjmp(state->db->jbuf, 1);\
     }\
 } while(0)
@@ -322,6 +328,12 @@ static unsigned geoip2_get_dclist(geoip2_t* db, const uint32_t offset) {
         },
         .out_of_data = false,
     };
+
+    // for 1.1.5+, we must subtract MMDB_DATA_SECTION_SEPARATOR
+    // (which maxminddb.c internally defines as the value 16) from
+    // the offset before calling functions like MMDB_aget_value()
+    if(libmmdb_gt_114)
+        state.entry.offset -= 16;
 
     uint32_t dclist = DCLIST_AUTO;
 
@@ -467,6 +479,13 @@ nlist_t* gdgeoip2_make_list(const char* pathname, const char* map_name, dclists_
     return nl;
 }
 
+void gdgeoip2_init(void) {
+    unsigned x, y, z;
+    if(sscanf(MMDB_lib_version(), "%3u.%3u.%3u", &x, &y, &z) == 3)
+        if(x > 1 || (x == 1 && (y > 1 || (y == 1 && z > 4))))
+            libmmdb_gt_114 = true;
+}
+
 #else // HAVE_GEOIP2
 
 nlist_t* gdgeoip2_make_list(const char* pathname, const char* map_name, dclists_t* dclists V_UNUSED, const dcmap_t* dcmap V_UNUSED, const bool city_auto_mode V_UNUSED, const bool city_no_region V_UNUSED) {
@@ -474,5 +493,7 @@ nlist_t* gdgeoip2_make_list(const char* pathname, const char* map_name, dclists_
     log_fatal("plugin_geoip: map '%s': GeoIP2 support needed by '%s' not included in this build!", map_name, pathname);
     return NULL; // unreachable
 }
+
+void gdgeoip2_init(void) { }
 
 #endif

libgdmaps/gdgeoip2.h
@@ -28,6 +28,8 @@
 
 #include <stdbool.h>
 
+void gdgeoip2_init(void);
+
 F_NONNULLX(1,2,3)
 nlist_t* gdgeoip2_make_list(const char* pathname, const char* map_name, dclists_t* dclists, const dcmap_t* dcmap, const bool city_auto_mode, const bool city_no_region);
 

libgdmaps/gdmaps.c
@@ -617,6 +617,8 @@ gdmaps_t* gdmaps_new(vscf_data_t* maps_cfg) {
     dmn_assert(maps_cfg);
     dmn_assert(vscf_is_hash(maps_cfg));
 
+    gdgeoip2_init();
+
     gdmaps_t* gdmaps = xcalloc(1, sizeof(gdmaps_t));
 
     vscf_data_t* crn_cfg = vscf_hash_get_data_byconstkey(maps_cfg, "city_region_names", true);

commit 3b56aafb0f0fc032b3362f7b3083d6a4b21fee75

Remove accidental debug code
Previous commit 56b9f14d accidentally committed some temporary
debugging code that was used while finding the issue in question

libgdmaps/gdgeoip2.c
@@ -212,8 +212,8 @@ static const char* GEOIP2_PATH_CITY[] = { "city", "names", "en", NULL };
         }\
     }\
     else if(mmrv_ != MMDB_LOOKUP_PATH_DOES_NOT_MATCH_DATA_ERROR) {\
-        dmn_log_err("plugin_geoip: map %s: Unexpected error fetching GeoIP2 data (%i: %s): Backtrace: %s",\
-            state->db->map_name, mmrv_, MMDB_strerror(mmrv_), dmn_logf_bt());\
+        dmn_log_err("plugin_geoip: map %s: Unexpected error fetching GeoIP2 data (%s)",\
+            state->db->map_name, MMDB_strerror(mmrv_));\
         siglongjmp(state->db->jbuf, 1);\
     }\
 } while(0)