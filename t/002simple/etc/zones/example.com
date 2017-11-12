gdnsd/gdnsd

commit b08e3e951e8c09b7067a2ce8e549eb877ee321e7

hashing cleanup: static lookup2 everywhere
We have a central (static, inline-able) implementation of lookup2
that was re-used in various places, and also multiple separate
implementations (with slight interface variations) of the
more-simplistic DJB hash (xor variant).

This rids the code of all DJB hash variants, replacing them all
with short wrappers around gdnsd_lookup2() which are static and
inline-able.  lookup2 can be slightly slower but it's a much
better hash.  In almost call cases we'd prefer its robustness, and
in 2016 the minor CPU perf diff between the two is trivial.

Note: as a special case, gdnsd_dname_hash also retains its library
implementation, so that external plugins don't need a re-compile.
The hacks around gdnsd_dname_hash_static can be removed in a
future major-version bump.

include/gdnsd/dname.h
@@ -23,6 +23,7 @@
 #include <gdnsd/compiler.h>
 #include <gdnsd/alloc.h>
 #include <gdnsd/dmn.h>
+#include <gdnsd/misc.h>
 
 #include <inttypes.h>
 #include <string.h>
@@ -212,10 +213,22 @@ static void gdnsd_dname_terminate(uint8_t* dname) {
 F_NONNULL F_PURE
 gdnsd_dname_status_t gdnsd_dname_status(const uint8_t* dname);
 
+// gdnsd_dname_hash was a library function, and must remain so for un-rebuilt
+// 3rd party plugins for now.  To cleanup in next major version bump...
 F_PURE F_NONNULL
 unsigned gdnsd_dname_hash(const uint8_t* input);
 
 #pragma GCC visibility pop
+
+// This static version of the above and #define lets core code and rebuilt
+// plugins use the static version instead, which can be inlined.
+F_PURE F_NONNULL F_UNUSED
+static unsigned gdnsd_dname_hash_static(const uint8_t *input) {
+    dmn_assert(input);
+    const uint32_t len = *input++ - 1U;
+    return gdnsd_lookup2(input, len);
+}
+#define gdnsd_dname_hash gdnsd_dname_hash_static
 
 // Check the status of a known-good dname.  It is assumed that the dname was
 //  constructed correctly by other code, and merely differentiates quickly

include/gdnsd/misc.h
@@ -208,7 +208,7 @@ static uint32_t gdnsd_rand32_get(gdnsd_rstate32_t* rs) {
     c -= a; c -= b; c ^= (b>>15); \
 }
 
-F_PURE F_UNUSED
+F_PURE F_UNUSED F_WUNUSED
 static uint32_t gdnsd_lookup2(const uint8_t *k, uint32_t len) {
     dmn_assert(k || !len);
 

libgdmaps/fips104.c
@@ -26,6 +26,7 @@
 #include <gdnsd/dmn.h>
 #include <gdnsd/log.h>
 #include <gdnsd/paths.h>
+#include <gdnsd/misc.h>
 
 #include <stdio.h>
 #include <string.h>
@@ -53,16 +54,9 @@ struct _fips_t {
 
 // keys are a uint32_t made of 4 bytes: CCRR (Country/Region)
 F_CONST
-static unsigned fips_djb_hash(uint32_t key) {
+static unsigned fips_hash(uint32_t key) {
    dmn_assert(key);
-
-   unsigned hash = 5381U;
-   hash = (hash * 33) ^ (key & 0xFFU);
-   hash = (hash * 33) ^ ((key & 0xFF00U) >> 8U);
-   hash = (hash * 33) ^ ((key & 0xFF0000U) >> 16U);
-   hash = (hash * 33) ^ ((key & 0xFF000000U) >> 24U);
-
-   return hash & FIPS_HASH_MASK;
+   return gdnsd_lookup2((const uint8_t*)&key, 4) & FIPS_HASH_MASK;
 }
 
 // It is assumed there are no duplicates in the input data.
@@ -73,7 +67,7 @@ static void fips_hash_add(fips_t* fips, const uint32_t key, const char* val) {
     dmn_assert(val);
 
     unsigned jmpby = 1;
-    unsigned slotnum = fips_djb_hash(key);
+    unsigned slotnum = fips_hash(key);
     while(fips->table[slotnum].key)
         slotnum = (slotnum + jmpby++) & FIPS_HASH_MASK;
     fips->table[slotnum].key = key;
@@ -115,7 +109,7 @@ const char* fips_lookup(const fips_t* fips, const uint32_t key) {
     dmn_assert(key);
 
     unsigned jmpby = 1;
-    unsigned slotnum = fips_djb_hash(key);
+    unsigned slotnum = fips_hash(key);
     while(fips->table[slotnum].key) {
         if(fips->table[slotnum].key == key)
             return fips->table[slotnum].val;

libgdnsd/dname.c
@@ -276,6 +276,8 @@ gdnsd_dname_status_t gdnsd_dname_status(const uint8_t* dname) {
     return llen ? DNAME_PARTIAL : DNAME_VALID;
 }
 
+// see dname.h re: static vs library variant
+#undef gdnsd_dname_hash
 uint32_t gdnsd_dname_hash(const uint8_t *k) {
     dmn_assert(k);
 

libgdnsd/vscf.rl
@@ -23,6 +23,7 @@
 #include <gdnsd/alloc.h>
 #include <gdnsd/dmn.h>
 #include <gdnsd/file.h>
+#include <gdnsd/misc.h>
 
 #include <stdlib.h>
 #include <stdarg.h>
@@ -129,15 +130,9 @@ static unsigned count2mask(unsigned x) {
 }
 
 F_NONNULL F_PURE
-static unsigned djb_hash(const char* k, unsigned klen, const unsigned hash_mask) {
+static unsigned key_hash(const char* k, unsigned klen, const unsigned hash_mask) {
    dmn_assert(k);
-
-   unsigned hash = 5381;
-
-   while(klen--)
-       hash = ((hash << 5) + hash) ^ (unsigned)*k++;
-
-   return hash & hash_mask;
+   return gdnsd_lookup2((const uint8_t*)k, klen) & hash_mask;
 }
 
 F_WUNUSED
@@ -157,7 +152,7 @@ static void hash_grow(vscf_hash_t* h) {
     for(unsigned i = 0; i <= old_hash_mask; i++) {
         vscf_hentry_t* entry = h->children[i];
         while(entry) {
-            const unsigned child_hash = djb_hash(entry->key, entry->klen, new_hash_mask);
+            const unsigned child_hash = key_hash(entry->key, entry->klen, new_hash_mask);
             vscf_hentry_t* slot = new_table[child_hash];
             vscf_hentry_t* next_entry = entry->next;
             entry->next = NULL;
@@ -192,7 +187,7 @@ static bool hash_add_val(const char* key, const unsigned klen, vscf_hash_t* h, v
     }
 
     const unsigned child_mask = count2mask(h->child_count);
-    const unsigned child_hash = djb_hash(key, klen, child_mask);
+    const unsigned child_hash = key_hash(key, klen, child_mask);
 
     vscf_hentry_t** store_at = &(h->children[child_hash]);
     while(*store_at) {
@@ -830,7 +825,7 @@ vscf_data_t* vscf_hash_get_data_bykey(const vscf_data_t* d, const char* key, uns
     dmn_assert(key);
     if(d->hash.child_count) {
         unsigned child_mask = count2mask(d->hash.child_count);
-        unsigned child_hash = djb_hash(key, klen, child_mask);
+        unsigned child_hash = key_hash(key, klen, child_mask);
         vscf_hentry_t* he = d->hash.children[child_hash];
         while(he) {
             if((klen == he->klen) && !memcmp(key, he->key, klen)) {
@@ -866,7 +861,7 @@ int vscf_hash_get_index_bykey(const vscf_data_t* d, const char* key, unsigned kl
     dmn_assert(key);
     if(d->hash.child_count) {
         unsigned child_mask = count2mask(d->hash.child_count);
-        unsigned child_hash = djb_hash(key, klen, child_mask);
+        unsigned child_hash = key_hash(key, klen, child_mask);
         vscf_hentry_t* he = d->hash.children[child_hash];
         while(he) {
             if((klen == he->klen) && !memcmp(key, he->key, klen))

src/dnspacket.c
@@ -1708,7 +1708,7 @@ static ltree_dname_status_t search_zone_for_dname(const uint8_t* dname, const zo
         const uint8_t* child_label = lstack[lcount];
         deleg_mod += *child_label;
         deleg_mod++;
-        ltree_node_t* entry = current->child_table[label_djb_hash(child_label, current->child_hash_mask)];
+        ltree_node_t* entry = current->child_table[ltree_hash(child_label, current->child_hash_mask)];
 
         while(entry) {
             if(!gdnsd_label_cmp(entry->label, child_label)) {
@@ -1723,7 +1723,7 @@ static ltree_dname_status_t search_zone_for_dname(const uint8_t* dname, const zo
     if(!rv_node && current->child_table) {
         dmn_assert(rval == DNAME_AUTH);
         static const uint8_t label_wild[2] =  { '\001', '*' };
-        ltree_node_t* entry = current->child_table[label_djb_hash(label_wild, current->child_hash_mask)];
+        ltree_node_t* entry = current->child_table[ltree_hash(label_wild, current->child_hash_mask)];
         while(entry) {
             if(entry->label[0] == '\001' && entry->label[1] == '*') {
                 rv_node = entry;

src/ltree.c
@@ -123,7 +123,7 @@ static void ltree_childtable_grow(ltree_node_t* node) {
             ltree_node_t* next_entry = entry->next;
             entry->next = NULL;
 
-            const uint32_t child_hash = label_djb_hash(entry->label, new_hash_mask);
+            const uint32_t child_hash = ltree_hash(entry->label, new_hash_mask);
             ltree_node_t* slot = new_table[child_hash];
 
             if(slot) {
@@ -152,7 +152,7 @@ static ltree_node_t* ltree_node_find_child(const ltree_node_t* node, const uint8
 
     if(node->child_table) {
         const uint32_t child_mask = count2mask(node->child_hash_mask);
-        const uint32_t child_hash = label_djb_hash(child_label, child_mask);
+        const uint32_t child_hash = ltree_hash(child_label, child_mask);
         ltree_node_t* child = node->child_table[child_hash];
         while(child) {
             if(!gdnsd_label_cmp(child_label, child->label)) {
@@ -183,7 +183,7 @@ static ltree_node_t* ltree_node_find_or_add_child(ltarena_t* arena, ltree_node_t
     dmn_assert(node); dmn_assert(child_label);
 
     const uint32_t child_mask = count2mask(node->child_hash_mask);
-    const uint32_t child_hash = label_djb_hash(child_label, child_mask);
+    const uint32_t child_hash = ltree_hash(child_label, child_mask);
 
     if(!node->child_table) {
         dmn_assert(!node->child_hash_mask);
@@ -820,7 +820,7 @@ static ltree_dname_status_t ltree_search_dname_zone(const uint8_t* dname, const 
 
             lcount--;
             const uint8_t* child_label = lstack[lcount];
-            ltree_node_t* entry = current->child_table[label_djb_hash(child_label, current->child_hash_mask)];
+            ltree_node_t* entry = current->child_table[ltree_hash(child_label, current->child_hash_mask)];
 
             while(entry) {
                 if(!gdnsd_label_cmp(child_label, entry->label)) {
@@ -834,7 +834,7 @@ static ltree_dname_status_t ltree_search_dname_zone(const uint8_t* dname, const 
         //  If in auth space with no match, and we still have a child_table, check for wildcard
         if(!rv_node && rval == DNAME_AUTH && current->child_table) {
             static const uint8_t label_wild[2] =  { '\001', '*' };
-            ltree_node_t* entry = current->child_table[label_djb_hash(label_wild, current->child_hash_mask)];
+            ltree_node_t* entry = current->child_table[ltree_hash(label_wild, current->child_hash_mask)];
             while(entry) {
                 if(entry->label[0] == '\001' && entry->label[1] == '*') {
                     rv_node = entry;

src/ltree.h
@@ -93,6 +93,7 @@ typedef struct _ltree_node_struct ltree_node_t;
 
 #include <gdnsd/compiler.h>
 #include <gdnsd/plugapi.h>
+#include <gdnsd/misc.h>
 
 #include <inttypes.h>
 #include <stdbool.h>
@@ -375,23 +376,13 @@ typedef enum {
     DNAME_DELEG = 2
 } ltree_dname_status_t;
 
-/********************************************************************
- * This is the excellent fast string hash algorithm DJB came up
- * with.  He uses it in his cdb database.  http://cr.yp.to
- ********************************************************************/
-
-// this variant is for labels encoded as one length-byte followed
-//  by N characters.  Thus the label "www" becomes "\003www" (4 bytes)
+// this hash wrapper is for labels encoded as one length-byte followed
+//  by N characters.  Thus the label "www" is "\003www" (4 bytes)
 F_UNUSED F_PURE F_WUNUSED F_NONNULL F_UNUSED
-static uint32_t label_djb_hash(const uint8_t* input, const uint32_t hash_mask) {
+static uint32_t ltree_hash(const uint8_t* input, const uint32_t hash_mask) {
    dmn_assert(input);
-
-   uint32_t hash = 5381;
-   uint32_t len = *input++;
-   while(len--)
-       hash = (hash * 33) ^ *input++;
-
-   return hash & hash_mask;
+   const unsigned len = *input++;
+   return gdnsd_lookup2(input, len) & hash_mask;
 }
 
 // "lstack" must be allocated to 127 pointers

commit a26cade73d0c3b31b9827e8c5fdb397a59a1c41a

fixup dnspacket.c coverage after hashing change
The hashing change to lookup2() in b08e3e95 caused the testsuite
to start missing an edge-case code branch in dnspacket.c, because
it re-ordered the internal hashtables in use during testing.

Adding this record here restores 100% branch coverage for
dnspacket.c.

t/002simple/etc/zones/example.com
@@ -31,6 +31,11 @@ ns2 A 192.0.2.6
 ns3.goober	A	192.0.2.7
 ns4.goober	A	192.0.2.8
 
+; this is here to exercise a rare branch in dnspacket.c
+; This case is looking for non-existent label (NXDOMAIN) within a level of the
+; name tree which contains only single-letter, non-wildcard labels...
+a.www		A	192.0.2.9
+
 _http._tcp	2W	srv	5 500 80 www
 
 @	TXT	"foo bar baz" "asdf 123 123 foo"