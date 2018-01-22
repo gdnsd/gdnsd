gdnsd/gdnsd

commit e53b7522d8c63e9ceb5905785e04b2dc19ce5b84

Remove excess newlines from a few log outputs

libgdnsd/file.c
@@ -95,7 +95,7 @@ gdnsd_fmap_t* gdnsd_fmap_new(const char* fn, const bool seq) {
     if(len) {
         mapbuf = mmap(NULL, len, PROT_READ, MAP_SHARED, fd, 0);
         if(mapbuf == MAP_FAILED) {
-            dmn_log_err("Cannot mmap '%s': %s\n", fn, dmn_logf_errno());
+            dmn_log_err("Cannot mmap '%s': %s", fn, logf_errno());
             close(fd);
             // cppcheck-suppress memleak (MAP_FAILED is not a leak :P)
             return NULL;
@@ -141,7 +141,7 @@ bool gdnsd_fmap_delete(gdnsd_fmap_t* fmap) {
     if(fmap->fd >= 0) {
         dmn_assert(fmap->len);
         if(munmap(fmap->buf, fmap->len) || close(fmap->fd)) {
-            dmn_log_err("Cannot munmap()/close() '%s': %s\n",
+            dmn_log_err("Cannot munmap()/close() '%s': %s",
                 fmap->fn, dmn_logf_errno());
             rv = true;
         }

libgdnsd/vscf.rl
@@ -222,7 +222,7 @@ static bool scnr_hash_add_val(vscf_scnr_t* scnr, vscf_hash_t* h, vscf_data_t* v)
         scnr->cur_klen = 0;
     }
     else {
-        parse_error("Duplicate hash key '%s'\n", scnr->cur_key);
+        parse_error("Duplicate hash key '%s'", scnr->cur_key);
     }
     return rv;
 }

commit 2cab0629b513897d995fc33de6af64c6535725f5

Fix fmap errno logging call
This was accidentally broken in the previous commit e53b7522 due
to interface leakage from the 3.x branches where the newlines were
first noticed.

libgdnsd/file.c
@@ -95,7 +95,7 @@ gdnsd_fmap_t* gdnsd_fmap_new(const char* fn, const bool seq) {
     if(len) {
         mapbuf = mmap(NULL, len, PROT_READ, MAP_SHARED, fd, 0);
         if(mapbuf == MAP_FAILED) {
-            dmn_log_err("Cannot mmap '%s': %s", fn, logf_errno());
+            dmn_log_err("Cannot mmap '%s': %s", fn, dmn_logf_errno());
             close(fd);
             // cppcheck-suppress memleak (MAP_FAILED is not a leak :P)
             return NULL;