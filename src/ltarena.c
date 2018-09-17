9e6eb6f9309c10ec39506d0025ffed1b42709e79
Brandon L Black <blblack@gmail.com>
2018-09-17 23:59:27 +0000

    Simplify ltarena code, kill de-duping hashtable
    I don't think the hashtable for dname de-duplication was doing
    much anymore; it probably had more utility back in the era when
    all zones shared storage.  It may help in some scenarios, but in
    most average-case zones it's just a waste.  While at it, moved the
    interfaces around a bit, instrumented with better attributes for
    bounds checking, cleaned up a few other odds and ends.

--- src/ltarena.c
+++ src/ltarena.c
@@ -35,9 +35,9 @@
 //   *some* amount of locality-of-reference to the related
 //   objects referencing the strings.
 // We initially reserve room in the ltarena object to track
-//   8 pools, which expands by doubling to support far more
+//   4 pools, which expands by doubling to support far more
 //   pools than needed by even the largest zones in existence.
-#define POOL_SIZE 512U // *must* be >= (256 + (red_size*2)) && multiple of 4
+#define POOL_SIZE 1024U // *must* be >= (256 + (red_size*2)) && multiple of 4
 #define INIT_POOLS_ALLOC 4U // *must* be 2^n && > 0
 
 // Normally, our pools are initialized to all-zeros for us
@@ -52,77 +52,11 @@
 #  define RED_SIZE 0
 #endif
 
-// INIT_DNHASH_MASK + 1 is the initial number of slots
-//   in the dnhash table, which grows by doubling every
-//   time the count of stored unique dnames reaches half
-//   the slot count.
-#define INIT_DNHASH_MASK 31U // *must* be 2^n-1 && > 0
-
-typedef struct {
-    unsigned count;
-    unsigned mask;
-    const uint8_t** table;
-} dnhash_t;
-
-F_MALLOC
-static dnhash_t* dnhash_new(void)
-{
-    gdnsd_assert(INIT_DNHASH_MASK);
-    gdnsd_assert(!((INIT_DNHASH_MASK + 1U) & INIT_DNHASH_MASK)); // 2^n-1
-
-    dnhash_t* rv = xmalloc(sizeof(*rv));
-    rv->count = 0;
-    rv->mask = INIT_DNHASH_MASK;
-    rv->table = xcalloc_n(INIT_DNHASH_MASK + 1U, sizeof(*rv->table));
-    return rv;
-}
-
-F_NONNULL
-static void dnhash_destroy(dnhash_t* dnhash)
-{
-    gdnsd_assert(dnhash->table);
-    gdnsd_assert(dnhash->mask);
-    free(dnhash->table);
-    free(dnhash);
-}
-
-// grow a dnhash_t's hashtable size by doubling
-F_NONNULL
-static void dnhash_grow(dnhash_t* dnhash)
-{
-    gdnsd_assert(dnhash->count);
-    // assert that dnhash->mask is still 2^n-1 and >0
-    gdnsd_assert(dnhash->mask);
-    gdnsd_assert(!((dnhash->mask + 1U) & dnhash->mask));
-
-    const uint8_t** old_table = dnhash->table;
-    const unsigned old_mask = dnhash->mask;
-    const unsigned new_mask = (old_mask << 1U) | 1U;
-    const uint8_t** new_table = xcalloc_n(new_mask + 1U, sizeof(*new_table));
-    for (unsigned i = 0; i <= old_mask; i++) {
-        const uint8_t* item = old_table[i];
-        if (item) {
-            unsigned jmpby = 1U;
-            unsigned new_slot = dname_hash(item) & new_mask;
-            while (new_table[new_slot]) {
-                new_slot += jmpby++;
-                new_slot &= new_mask;
-            }
-            new_table[new_slot] = item;
-        }
-    }
-
-    free(dnhash->table);
-    dnhash->table = new_table;
-    dnhash->mask = new_mask;
-}
-
 struct _ltarena {
     uint8_t** pools;
-    unsigned pool;
-    unsigned poffs;
-    unsigned palloc;
-    dnhash_t* dnhash;
+    size_t pool;
+    size_t poffs;
+    size_t palloc;
 };
 
 static void* make_pool(void)
@@ -134,7 +68,7 @@ static void* make_pool(void)
         // malloc + fill in deadbeef if using redzones
         p = xmalloc(POOL_SIZE);
         uint32_t* p32 = p;
-        unsigned idx = POOL_SIZE >> 2U;
+        size_t idx = POOL_SIZE >> 2U;
         while (idx--)
             p32[idx] = 0xDEADBEEF;
     } else {
@@ -156,23 +90,18 @@ ltarena_t* lta_new(void)
     rv->palloc = INIT_POOLS_ALLOC;
     rv->pools = xmalloc_n(INIT_POOLS_ALLOC, sizeof(*rv->pools));
     rv->pools[0] = make_pool();
-    rv->dnhash = dnhash_new();
     return rv;
 }
 
 void lta_close(ltarena_t* lta)
 {
-    if (lta->dnhash) {
-        dnhash_destroy(lta->dnhash);
-        lta->dnhash = NULL;
-        lta->pools = xrealloc_n(lta->pools, lta->pool + 1, sizeof(*lta->pools));
-    }
+    lta->pools = xrealloc_n(lta->pools, lta->pool + 1, sizeof(*lta->pools));
 }
 
 void lta_destroy(ltarena_t* lta)
 {
     lta_close(lta);
-    unsigned whichp = lta->pool + 1U;
+    size_t whichp = lta->pool + 1U;
     while (whichp--) {
         VALGRIND_DESTROY_MEMPOOL(lta->pools[whichp]);
         free(lta->pools[whichp]);
@@ -181,11 +110,9 @@ void lta_destroy(ltarena_t* lta)
     free(lta);
 }
 
-F_MALLOC F_NONNULL
-static uint8_t* lta_malloc(ltarena_t* lta, const unsigned size)
+uint8_t* lta_malloc(ltarena_t* lta, const size_t size)
 {
     gdnsd_assert(size);
-    gdnsd_assert(lta->dnhash); // not closed
 
     // Currently, all allocations obey this assertion.
     // Only labels + dnames are stored here, which max out at 256
@@ -193,7 +120,7 @@ static uint8_t* lta_malloc(ltarena_t* lta, const unsigned size)
 
     // the requested size + redzones on either end, giving the total
     //   this allocation will steal from the pool
-    const unsigned size_plus_red = size + RED_SIZE + RED_SIZE;
+    const size_t size_plus_red = size + RED_SIZE + RED_SIZE;
 
     // this could be a compile-time check, just stuffing here instead for now
     gdnsd_assert(POOL_SIZE >= (256 + RED_SIZE + RED_SIZE));
@@ -222,41 +149,4 @@ static uint8_t* lta_malloc(ltarena_t* lta, const unsigned size)
         memset(rval, 0, size);
 
     return rval;
-}
-
-uint8_t* lta_labeldup(ltarena_t* lta, const uint8_t* label)
-{
-    const unsigned sz = *label + 1U;
-    uint8_t* rv = lta_malloc(lta, sz);
-    memcpy(rv, label, sz);
-    return rv;
-}
-
-// this mixes internal access to dnhash_t as well, so it's not
-//   properly a just method of ltarena_t in that sense.
-const uint8_t* lta_dnamedup(ltarena_t* lta, const uint8_t* dname)
-{
-    dnhash_t* dnhash = lta->dnhash;
-    gdnsd_assert(dnhash); // not closed
-
-    const unsigned hmask = dnhash->mask;
-    const uint8_t** table = dnhash->table;
-    uint32_t jmpby = 1U;
-    uint32_t slotnum = dname_hash(dname) & hmask;
-    while (table[slotnum]) {
-        if (!gdnsd_dname_cmp(dname, table[slotnum]))
-            return table[slotnum];
-        slotnum += jmpby++;
-        slotnum &= hmask;
-    }
-
-    const unsigned dnlen = *dname + 1U;
-    uint8_t* retval = lta_malloc(lta, dnlen);
-    table[slotnum] = retval;
-    memcpy(retval, dname, dnlen);
-
-    if (++dnhash->count > (dnhash->mask >> 1U))
-        dnhash_grow(dnhash);
-
-    return retval;
 }
--- src/ltarena.h
+++ src/ltarena.h
@@ -23,9 +23,10 @@
 #include <gdnsd/compiler.h>
 
 #include <inttypes.h>
+#include <string.h>
 
 /******************************************************************\
-* ltarena is arena storage for ltree string data, it allocates
+* ltarena is arena storage for ltree label/dname data, it allocates
 *   unaligned string data in giant pools with no overhead.
 \******************************************************************/
 
@@ -35,19 +36,24 @@ typedef struct _ltarena ltarena_t;
 F_WUNUSED
 ltarena_t* lta_new(void);
 
-// This is like a strdup() that allocates from an lta arena
-//  and happens to know the internally-encoded length of
-//  label strings as used in ltrees.
-// Use only for label data.
-F_MALLOC F_NONNULL
-uint8_t* lta_labeldup(ltarena_t* lta, const uint8_t* label);
+// Actual allocator, asserts size <= 256, cannot fail or return nonnull
+// (underlying allocator will abort the whole program if truly unable to make
+// space for new pools)
+F_MALLOC F_ALLOCSZ(2) F_RETNN F_NONNULL
+uint8_t* lta_malloc(ltarena_t* lta, const size_t size);
 
-// As above, except:
-//   1) Assumes gdnsd's "dname"-format data
-//   2) Can de-duplicate storage by returning data that aliases
-//     previous allocations via this same interface and arena.
-F_WUNUSED F_NONNULL
-const uint8_t* lta_dnamedup(ltarena_t* lta, const uint8_t* dname);
+// Duplicate a label in arena storage
+F_UNUSED F_RETNN F_NONNULL
+static uint8_t* lta_labeldup(ltarena_t* lta, const uint8_t* label)
+{
+    const size_t sz = *label + 1U;
+    uint8_t* rv = lta_malloc(lta, sz);
+    memcpy(rv, label, sz);
+    return rv;
+}
+
+// As above for dnames, function is identical, but naming matters
+#define lta_dnamedup lta_labeldup
 
 // Close an arena to further allocations, idempotent.
 // After this call, the only valid operations are _close()/_destroy()
