9dbe10048aa5a5494d9774e4b01c3b3161b0eec8
Brandon L Black <blblack@gmail.com>
2019-02-09 17:04:00 +0000

    Restore branch coverage for Sonar
    I had branch coverage working for Sonar at one point during
    testing, but it's broken in the latest uploads.  Hopefully it's
    just that we need "-b" in addition to "-c" to get it going again.

--- qa/travis-build.sh
+++ qa/travis-build.sh
@@ -28,7 +28,7 @@ case "$GDNSD_TRAVIS_BUILD" in
     sonarcloud)
         CFLAGS="-O0 -g -fprofile-arcs -ftest-coverage" CPPFLAGS="-DGDNSD_NO_UNREACH_BUILTIN -DGDNSD_NO_FATAL_COVERAGE -DGDNSD_COVERTEST_EXIT" ./configure --without-hardening
         SLOW_TESTS=1 make -j$TEST_CPUS check
-        gcov -a -c -p src/*.o src/plugins/*.o libgdmaps/*.o libgdnsd/*.o
+        gcov -a -b -c -p src/*.o src/plugins/*.o libgdmaps/*.o libgdnsd/*.o
         make clean
         ./configure --disable-developer --without-hardening
         build-wrapper-linux-x86-64 --out-dir bw-output make -j$TEST_CPUS
