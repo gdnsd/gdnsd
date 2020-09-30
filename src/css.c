20acc994ac9b378ede4f1fce0015b2f6a8e2cd98
Ryan Steinmetz <zi@FreeBSD.org>
2020-09-30 13:07:58 -0400

    - Add SOL_TCP workaround from src/dnsio_tcp.c to fix build (#194)
    (cherry picked from commit 8756d39f9afee3c31da05dc6f985b5cc62fda538)

--- src/css.c
+++ src/css.c
@@ -932,6 +932,10 @@ static void socks_import_fds(const socks_cfg_t* socks_cfg, const int* fds, const
         socks_import_fd(socks_cfg, fds[i]);
 }
 
+#ifndef SOL_TCP
+#define SOL_TCP IPPROTO_TCP
+#endif
+
 F_NONNULL F_WUNUSED
 static int make_tcp_listener_fd(const gdnsd_anysin_t* addr)
 {
