d2c188be7b371724667cd36f498b7d41b92a51ac
Brandon L Black <blblack@gmail.com>
2021-01-31 11:15:44 +0000

    Replace JKISS32 with JKISS
    JKISS is a bit higher in quality than JKISS32 (although still not
    cryptographic, which is fine!) at the cost of a couple multiplies,
    and it's really not a big speed hit to use it.  Also, I'm a little
    more confident in how we seed this variant properly to avoid edge
    cases.  We have more use-cases coming and I'm starting to care
    more about its quality affecting algorithms whose properties rely
    on "random" choices.

--- include/gdnsd/rand.h
+++ include/gdnsd/rand.h
@@ -29,7 +29,7 @@
 #include <sodium.h>
 
 /***************
- * This is the Public-Domain JKISS32 PRNG implementation which I initially got
+ * This is the Public-Domain JKISS PRNG implementation which I initially got
  *   from David Jones' RNG paper here:
  *   http://www.cs.ucl.ac.uk/staff/d.jones/GoodPracticeRNG.pdf
  *   ... and then incorporated some usage/optimization hints from
@@ -45,36 +45,39 @@ struct rstate32 {
     uint32_t x;
     uint32_t y;
     uint32_t z;
-    uint32_t w;
     uint32_t c;
 };
 
 F_NONNULL F_UNUSED
-static void gdnsd_rand32_init(struct rstate32* st)
-{
-    do {
-        randombytes_buf(st, sizeof(*st));
-    } while (!st->y); // y==0 is bad for jkiss32
-}
-
-F_NONNULL F_UNUSED
 static uint32_t gdnsd_rand32_get(struct rstate32* rs)
 {
+    rs->x = 314527869U * rs->x + 1234567U;
     uint32_t y = rs->y;
     y ^= y << 5;
     y ^= y >> 7;
     y ^= y << 22;
     rs->y = y;
+    uint64_t t = 4294584393ULL * rs->z + rs->c;
+    rs->c = t >> 32;
+    rs->z = t;
+    return rs->x + rs->y + rs->z;
+}
 
-    // Note local mods to how t is handled (results are the same)
-    uint32_t t = rs->z + rs->w + rs->c;
-    rs->z = rs->w;
-    rs->c = (t & 1U << 31) >> 31;
-    rs->w = t & 2147483647;
-
-    rs->x += 1411392427;
-
-    return rs->x + y + rs->w;
+F_NONNULL F_UNUSED
+static void gdnsd_rand32_init(struct rstate32* st)
+{
+    // Re-do initialization until we get all fields non-zero, and ensure that
+    // "c" does not exceed its natural limits from the multiply operation in
+    // the actual algorithm above (actually looping here should be exceedingly
+    // rare if randombytes_buf() is decent!)
+    do {
+        randombytes_buf(st, sizeof(*st));
+    } while (!st->x || !st->y || !st->z || !st->c || st->c >= 4294584393U);
+    // Warm up the PRNG as a safety measure, too, with a random cycle count
+    // of ~2K-4K iterations.
+    uint32_t cycles = 2048U + (randombytes_random() & 0x7FF);
+    while (cycles--)
+        gdnsd_rand32_get(st);
 }
 
 // Unbiased while avoiding div/mod ops most of the time for smaller bounds, and
