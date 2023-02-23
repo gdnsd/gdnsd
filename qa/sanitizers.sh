6404015172573a4abc21f4ba34f166e763fa923d
Brandon L Black <blblack@gmail.com>
2023-02-23 20:44:07 +0000

    Minor QA infra updates
    This removes some autconf'd warning flags which are included by
    default (or in Wall or Wextra) in modern compilers, adds Wmost and
    Wtautological-compare for clang, and adds some newer sanitizer
    checks as well

--- qa/sanitizers.sh
+++ qa/sanitizers.sh
@@ -5,11 +5,11 @@ if [ ! -f $PWD/qa/gdnsd.supp ]; then
    exit 99
 fi
 
-# Note this uses gcc-8's sanitizers, this probably won't work with earlier gcc versions or other variants
+# Note this uses gcc-10's sanitizers, this probably won't work with earlier gcc versions or other variants
 
 set -x
 set -e
 export ASAN_OPTIONS="check_initialization_order=true:detect_invalid_pointer_pairs=10:strict_string_checks=true:detect_stack_use_after_return=true"
-CFLAGS="-O1 -fno-omit-frame-pointer -fno-common -fno-sanitize-recover=all -fsanitize=address -fsanitize=leak -fsanitize=undefined -fsanitize=float-divide-by-zero -fsanitize=float-cast-overflow -fsanitize-address-use-after-scope" CC=gcc ./configure --enable-developer --without-hardening
+CFLAGS="-O1 -fno-omit-frame-pointer -fno-common -fno-sanitize-recover=all -fsanitize=address -fsanitize=leak -fsanitize=undefined -fsanitize=float-divide-by-zero -fsanitize=float-cast-overflow -fsanitize=bounds -fsanitize=alignment -fsanitize=object-size -fsanitize-address-use-after-scope -fsanitize=pointer-compare -fsanitize=pointer-subtract -fsanitize=signed-integer-overflow" CC=gcc ./configure --enable-developer --without-hardening
 make clean
 SLOW_TESTS=1 make check
