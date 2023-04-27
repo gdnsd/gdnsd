91ab22035f649f2426c4ae7150097bc8538ce959
Brandon L Black <blblack@gmail.com>
2023-04-27 19:18:37 +0000

    qa/ggcclyzer.sh: QA script for gcc -fanalyzer

---
+++ qa/gcclyzer.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# run from top of repo
+# does gcc -fanalyzer check with warnings upgraded to errors
+if [ ! -f $PWD/qa/gdnsd.supp ]; then
+   echo "Run this from the root of the source tree!"
+   exit 99
+fi
+set -x
+set -e
+CFLAGS="-fanalyzer -fanalyzer-verbosity=0 -fanalyzer-checker=taint -fanalyzer-transitivity" ./configure --enable-developer --without-hardening --with-werror
+make clean all
