7498ff05a0b0fb57f4b026c1761b403ba1c6fbb3
Brandon L Black <blblack@gmail.com>
2024-02-14 10:18:57 -0600

    style fixup
    A tab slipped in again, in the f0c4bfa17 qsort commit
    
    (cherry picked from commit 9b7fbb8d89bc10454138c3e0fe17a2060b8a9b24)

--- libgdmaps/nlist.c
+++ libgdmaps/nlist.c
@@ -137,7 +137,7 @@ static int net_sorter(const void* a_void, const void* b_void)
     gdnsd_assume(b->mask <= 0xFF);
     const int mcrv = memcmp(a->ipv6, b->ipv6, 16);
     if (mcrv)
-	return mcrv;
+        return mcrv;
     const int am = (int)a->mask;
     const int bm = (int)b->mask;
     return (am > bm) - (am < bm);
